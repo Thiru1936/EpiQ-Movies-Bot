7915a5ce1cda6eaca656e8de3aab934b62cd246b
Tamilgram <107699896+Tamilgram@users.noreply.github.com>
2025-03-28 17:11:33 +0530

    Update file_receiver.py

--- handlers/file_receiver.py
+++ handlers/file_receiver.py
@@ -5,30 +5,33 @@ from pyrogram.handlers import MessageHandler
 from common import batch_files, is_batch
 from configs import Config
 
-async def temp(_, __):
-    pass
+batch_files = {}
 
 async def receive_files(client, message):
     is_batch = True
     msg_id = message.id
     batch_files[msg_id] = []
-    async def event_filter(_, __, message):
-        print(message.text)
-        if message and message.text:
-            text = message.text
-            print(message.command)
+
+    async def event_filter(__, msg):
+        nonlocal is_batch 
+        if msg and msg.text:
+            text = msg.text
             if text.startswith('/cancel'):
                 is_batch = False
-                del batch_files[msg_id]
+                batch_files.pop(msg_id, None)
+                return
             elif text.startswith('/done'):
                 is_batch = False
-        batch_files[msg_id].append(message)
-        await message.delete()
-    handler = client.add_handler(MessageHandler(temp, filters=create(event_filter) & chat(Config.BOT_OWNER)), group=-1)
+
+        batch_files[msg_id].append(msg)
+        await msg.delete()
+
+    handler = MessageHandler(event_filter, chat(Config.BOT_OWNER))
+    client.add_handler(handler, group=-1)
+
     while is_batch:
         await sleep(0.5)
-    client.remove_handler(*handler)
-    try:
-        return batch_files[msg_id]
-    except:
-        return []
+
+    client.remove_handler(handler, group=-1)
+
+    return batch_files.pop(msg_id, [])
