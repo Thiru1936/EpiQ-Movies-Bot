221d51ede452bebbadc76424e572db2132135d74
Thiru1936 <thiru190609@gmail.com>
2025-06-26 22:44:30 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
   API_ID = int(os.environ.get("API_ID", "7264516826"))
   API_HASH = os.environ.get("API_HASH", "6845600d24b8760ab0c23a352e893222")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", " 7264516826:AAFVr9DTouZPRHAgHZrnAN1MZY1-jTxaN90")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "7264516826:AAHFm4RqXR-kxUW1mZh7Uhzy5Xjmjyxapls")
   BOT_USERNAME = os.environ.get("BOT_USERNAME", "Epiq_movies_bot")
   DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001785733558"))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "dashboard.gyanilinks.com")
